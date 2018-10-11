eb0bf35f6fc776b42bfe2a64d05f75b98ee66f6c
Ulug'bek <ubstart707@gmail.com>
2018-10-11 21:17:30 +0500

    tugallanmaga salah time

--- stime.py
+++ stime.py
@@ -3,28 +3,15 @@ import requests
 
 s = requests.get('https://islom.uz/')
 b = bs4.BeautifulSoup(s.text, "html.parser")
-print (b.select('.pray-times .a-link-white')[0].getText())
-print (b.select('.pray-times .a-link-white')[1].getText())
-print (b.select('.pray-times .a-link-white')[2].getText())
-print (b.select('.pray-times .a-link-white')[3].getText())
-print (b.select('.pray-times .a-link-white')[4].getText())
-print (b.select('.pray-times .a-link-white')[5].getText())
-print ("\n")
+print(s)
 
-# `.pray-times
-# .a-link-white
-
-
-
-
-
-
-
-// this tis comment 
-
-
-# url = "http://www.old.islom.uz/azon/gettable.php?GMT=26&type=1&alphatype=3&bgcolor=FFFFFF&background=&bordercolor=666666&bordergcolor1=FFFFFF&bordergcolor2=CCCCCC&titlebg=999999&titlecolor=FFFFFF&fontcolor=000000&fontsize=11"
-
-# findthis = "<u>ВРЕМЯ НАМАЗОВ</u>"
-# soup = bs4.BeautifulSoup(findthis, 'lxml')
-# print (soup.text)
+time = b.find_all('div', class_ = "p_clock " )
+print(time)
+print(type(time))
+#print (b.select('.p_clock .a-link-white')[0].getText())
+# print (b.select('.pray-times .a-link-white')[1].getText())
+# print (b.select('.pray-times .a-link-white')[2].getText())
+# print (b.select('.pray-times .a-link-white')[3].getText())
+# print (b.select('.pray-times .a-link-white')[4].getText())
+# print (b.select('.pray-times .a-link-white')[5].getText())
+# print ("\n")
