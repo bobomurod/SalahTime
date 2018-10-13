5571cf63baaeffb2d88c5a4fcbe2243a58018f44
Ulug'bek <ubstart707@gmail.com>
2018-10-13 07:23:55 +0500

    namoz vaqtlari ishi tugadi

--- ss.py
+++ ss.py
@@ -16,3 +16,30 @@ del namaazTimes[1]
 for namaazName, namaazTime in zip(namaazNames, namaazTimes):
     with open(path + namaazName + '.txt', 'w') as file:
         file.write(namaazTime)
+
+with open("Тонг.txt", "r") as my_new_handle:
+    for the_line in my_new_handle:
+# Do something with the line we just read.
+# Here we just print it.
+        print("Bomdot",the_line)
+with open("Пешин.txt", "r") as my_new_handle:
+    for the_line in my_new_handle:
+# Do something with the line we just read.
+# Here we just print it.
+        print("Пешин:",the_line)
+with open("Аср.txt", "r") as my_new_handle:
+    for the_line in my_new_handle:
+# Do something with the line we just read.
+# Here we just print it.
+        print("Аср:",the_line)
+with open("Шом.txt", "r") as my_new_handle:
+    for the_line in my_new_handle:
+# Do something with the line we just read.
+# Here we just print it.
+        print("Шом:",the_line)
+with open("Хуфтон.txt", "r") as my_new_handle:
+    for the_line in my_new_handle:
+# Do something with the line we just read.
+# Here we just print it.
+        print("Хуфтон:",the_line)
+        
