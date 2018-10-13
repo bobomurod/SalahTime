c092e7ea4c8389185f8dd2ecbbe5a63c73881c30
Ulug'bek <ubstart707@gmail.com>
2018-10-13 07:12:14 +0500

    namoz vaaqtlari olib faylga yozib beradi

--- ss.py
+++ ss.py
@@ -1,14 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.hujjat.org/'
+url = 'https://www.islom.uz/'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 path = '/home/ulugbek/python/SalahTime/'
 
-namaazNames = soup.select('div.NamaazTimeName')
+namaazNames = soup.select('div.p_v')
 namaazNames = [namaazName.text for namaazName in namaazNames]
-namaazTimes = soup.select('div.NamaazTime')
+namaazTimes = soup.select('div.p_clock')
 namaazTimes = [namaazTime.text for namaazTime in namaazTimes]
 del namaazNames[1]
 del namaazTimes[1]
